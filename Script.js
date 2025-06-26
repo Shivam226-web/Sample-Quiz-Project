c3effa01ef9e44fcf11ef6d2eb333ddaf6b4e3d8
Shivam226 <indrajeet7318116027@gmail.com>
2025-06-26 09:51:04 +0530

    Scripting part done

--- Script.js
+++ Script.js
@@ -60,13 +60,18 @@ const nextButton = document.getElementById("next-btn");
     resetState();
     let currentQuestion = questions[currentQuestionIndex];
     let questionNo = currentQuestionIndex + 1;
-    questionElement.innerHTML = questionNo + "." + currentQuestion.
-    question;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
     currentQuestion.answers.forEach(answer=>{
         const button = document.createElement("button");
         button.innerHTML = answer.text;
         button.classList.add("btn");
         answerButtons.appendChild(button);
+        if(answer.correct){
+            button.dataset.correct = answer.correct;
+
+        }
+        button.addEventListener("click" , selectAnswer);
     })
  }
   function resetState(){
@@ -75,4 +80,48 @@ const nextButton = document.getElementById("next-btn");
         answerButtons.removeChild(answerButtons.firstChild);
     }
   }
+  function selectAnswer(e){
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+
+    if(isCorrect){
+        selectedBtn.classList.add("correct");
+        score++;
+    }else {
+        selectedBtn.classList.add("incorrect");
+    }
+    Array.from(answerButtons.children).forEach(button => {
+        if(button.dataset.correct === "true"){
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    });
+    nextButton.style.display = "block";
+  }
+
+  function showScore(){
+    resetState();
+    questionElement.innerHTML =`your scored ${score} out of ${questions.length}`;
+    nextButton.innerHTML = "Play Again";
+    nextButton.style.display = "block";
+
+  }
+
+
+  function handleNextButton(){
+    currentQuestionIndex++;
+    if(currentQuestionIndex < questions.length){
+        showQuestion();
+    }else{
+        showScore();
+    }
+  }
+
+  nextButton.addEventListener("click", ()=>{
+    if(currentQuestionIndex < questions.length){
+        handleNextButton();
+    }else{
+        startQuize();
+    }
+  })
  startQuize();
